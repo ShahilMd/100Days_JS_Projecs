58561a57725d81853a4d5f4f745f24e0143cae79
Md Shahil <shahil.connect@gmail.com>
2025-03-10 12:34:24 +0000

    more javascript

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 function updateClock(){
 const hours = document.getElementById('hours');
-
+const minutes = document.getElementById('minutes')
 const second =document.getElementById('seconds');
 const ampm =document.getElementById('ampm');
+const date = document.getElementById('date');
 const time =new Date()
 
-const hrs = time.getHours()
-const min =
-console.log(hrs);
+const oldHrs = time.getHours()
+const newHrs= (oldHrs%12).toString().padStart(2,'0');
+const newDate = time() 
 
+
+
+const min = time.getMinutes().toString().padStart(2,'0')
+const sec = time.getSeconds().toString().padStart(2,'0')
+
+const amPM = oldHrs<=12 ? 'AM' :'PM'
+
+
+
+hours.textContent=newHrs
+minutes.textContent=min
+second.textContent=sec
+ampm.textContent=amPM
+date.textContent=newDate
 
 
 }
 
-setTimeout(updateClock)
+setInterval(updateClock)
