db727f7c474b8a6ce8d8b0b79b5644acac9b808a
Md Shahil <shahil.connect@gmail.com>
2025-03-10 12:03:19 +0000

    basic boiler plate

---
+++ index.js
@@ -0,0 +1,16 @@
+function updateClock(){
+const hours = document.getElementById('hours');
+
+const second =document.getElementById('seconds');
+const ampm =document.getElementById('ampm');
+const time =new Date()
+
+const hrs = time.getHours()
+const min =
+console.log(hrs);
+
+
+
+}
+
+setTimeout(updateClock)
